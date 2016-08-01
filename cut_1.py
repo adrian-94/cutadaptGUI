1ad1bac9d50321defaafad0961cb108c16440ae3
highness94 <adrianpereira1994@gmail.com>
2016-08-01 08:20:09 -0500

    trimming
    trimming done

--- cut_1.py
+++ cut_1.py
@@ -29,76 +29,101 @@ class Cutadapt(QtGui.QMainWindow, form_class):
         super(Cutadapt, self).__init__(*args)
         self.setupUi(self)
         self.prompt=0
+        self.prompt_1=0
         self.file_1=""
         self.file_2=""
         self.command=""
+        self.command_1=""
+        self.type=""
+        self.adapter=""
+        self.input_file=""
+        self.output_file=""
+        self.min_length="0"
+        self.max_length="150"
+        
+        self.too_short=" --too-short-output too_short_Output"
+        self.too_long=" --too-long-output too_long_Output"
+        self.untrimmed_output=""
+        self.discard_t=""
+        self.discard_unt=""
+        self.max_error="0.1"
+        self.bases="0"
+        
         self.frame.hide()
         self.frame_2.hide()
-        """
-        self.label_4.hide()
-        self.lineEdit_3.hide()
-        self.label_6.hide()
-        self.pushButton_2.hide()
-        self.lineEdit_5.hide()
-        #self.label_5.hide()
-        #self.lineEdit_4.hide() 
-        self.pushButton_6.hide()
-        self.pushButton_4.hide()
-        self.label_10.hide()#demulthide
-        self.label_7.hide()
-        self.groupBox.hide()
-        self.label_8.hide()
-        self.checkBox_3.hide()
-        self.pushButton.hide()
-        self.lineEdit_6.hide()
-        self.label_9.hide()
-        self.pushButton_3.hide()
-        self.lineEdit_3.hide()
-        self.label_11.hide()
-        self.lineEdit_9.hide()
-        self.lineEdit_7.hide()
-        self.label_14.hide()
-        self.checkBox_4.hide()
-        self.lineEdit_10.hide()
-        self.checkBox_6.hide()
-        self.lineEdit_11.hide()
-        self.checkBox_7.hide()
-        self.checkBox_8.hide()
-        self.lineEdit_13.hide()
-        self.lineEdit_12.hide()
-        self.checkBox_9.hide()
-        self.lineEdit_14.hide()
-        self.checkBox_10.hide()
-        self.checkBox_11.hide()
-        self.label_2.hide()
-        self.lineEdit.hide()
-        self.checkBox.hide()
-        self.label_3.hide()
-        #self.buttonBox.hide()
-        #self.pushButton_7.hide()
-        #self.pushButton_8.hide()
-        self.lineEdit_2.hide()
-        self.pushButton_5.hide()#trimHide
-        """
         self.checkBox_2.stateChanged.connect(self.checkBox_2_stateChanged)
         self.checkBox_5.stateChanged.connect(self.checkBox_5_stateChanged)
         self.checkBox_4.stateChanged.connect(self.checkBox_4_stateChanged)
         self.checkBox_6.stateChanged.connect(self.checkBox_6_stateChanged)
         self.checkBox_7.stateChanged.connect(self.checkBox_7_stateChanged)
         self.checkBox_8.stateChanged.connect(self.checkBox_8_stateChanged)
-        self.checkBox_9.stateChanged.connect(self.checkBox_9_stateChanged)
+        
         self.checkBox_10.stateChanged.connect(self.checkBox_10_stateChanged)
         self.pushButton_6.clicked.connect(self.browseAdapter)
         self.pushButton_2.clicked.connect(self.browseInput)
         self.pushButton_4.clicked.connect(self.deMulti)
+        self.checkBox_3.stateChanged.connect(self.adapterType)
+        self.pushButton.clicked.connect(self.adapterFile)
+        self.pushButton_3.clicked.connect(self.inputFile)
+        self.pushButton_5.clicked.connect(self.trimming)
+        self.radioButton.clicked.connect(self.adpt_3)
+        self.radioButton_2.clicked.connect(self.adpt_5)
+        self.radioButton_5.clicked.connect(self.adpt_anch5)
+        self.radioButton_3.clicked.connect(self.adpt_anch3)  
+        self.radioButton_6.clicked.connect(self.linked)
+       
+        
+        
+    def linked(self):
+        self.type="a"
+        
+    def adpt_anch3(self):
+        self.type="a"
     
-    def browseInput(self):
+    def adpt_anch5(self):
+        self.type="g"
+
+    def adpt_5(self):
+        self.type="g"
+        
+    def adpt_3(self):
+        self.type="a"
+        
+    
+        
+    def adapterType(self):
+        
+       self.prompt_1=self.checkBox_3.checkState()
+       if self.prompt_1==2:
+           self.pushButton.hide()
+       if self.prompt_1!=2:
+           self.pushButton.show()
+           
+    def adapterFile(self):
         
         caption="Open File"
         directory='./'
         #filter_mask="fastq files (*.fastq)"
         
-        self.file_1=(QFileDialog.getOpenFileNames(None, caption, directory))[0]
+        self.adapter=(QFileDialog.getOpenFileNames(None, caption, directory))[0]
+        self.lineEdit_6.setText(str(os.path.basename(self.adapter))) 
+        
+    def inputFile(self):
+        
+        caption="Open File"
+        directory='./'
+        filter_mask="fastq files (*.fastq)"
+        
+        self.input_file=(QFileDialog.getOpenFileNames(None, caption, directory, filter_mask))[0]
+        self.lineEdit_7.setText(str(os.path.basename(self.input_file)))
+        
+    def browseInput(self):
+        
+        caption="Open File"
+        directory='./'
+        filter_mask="fastq files (*.fastq)"
+        
+        self.file_1=(QFileDialog.getOpenFileNames(None, caption, directory, filter_mask))[0]
         self.lineEdit_5.setText(str(os.path.basename(self.file_1)))
         
         
@@ -118,18 +143,7 @@ class Cutadapt(QtGui.QMainWindow, form_class):
         subprocess.call("~/.local/bin/cutadapt -a file:"+self.file_2+" --no-trim --untrimmed-o noadapter_"+os.path.basename(self.file_1)+".gz -o {name}_"+os.path.basename(self.file_1)+".gz "+self.file_1, shell=True)
         
     def demultiHide(self):
-        """
-        self.label_4.hide()
-        self.lineEdit_3.hide()
-        self.label_6.hide()
-        self.pushButton_2.hide()
-        self.lineEdit_5.hide()
-        #self.label_5.hide()
-        #self.lineEdit_4.hide() 
-        self.label_10.hide()
-        self.pushButton_6.hide()
-        self.pushButton_4.hide()
-        """
+   
         self.frame.hide()
         
     def checkBox_2_stateChanged(self):
@@ -163,21 +177,13 @@ class Cutadapt(QtGui.QMainWindow, form_class):
        self.lineEdit_10.hide()
        self.checkBox_6.hide()
         
-    def checkBox_6_stateChanged(self):
+    def checkBox_6_stateChanged(self):#too short output
        self.prompt=self.checkBox_6.checkState()
        if self.prompt==2:
-           self.Shown_2()
-       if self.prompt!=2:
-           self.Hide_2()
+           self.too_short+="_"+str(os.path.basename(self.input_file))+" "
+      
            
-    def Shown_2(self):
-       
-       self.lineEdit_11.show()
-       
-       
-    def Hide_2(self):
-        
-       self.lineEdit_11.hide()
+    
        
        
     def checkBox_7_stateChanged(self):
@@ -197,41 +203,21 @@ class Cutadapt(QtGui.QMainWindow, form_class):
        self.lineEdit_12.hide()
        self.checkBox_8.hide()
        
-    def checkBox_8_stateChanged(self):
+    def checkBox_8_stateChanged(self):#too long output
         
        self.prompt=self.checkBox_8.checkState()
        if self.prompt==2:
-           self.Shown_4()
-       if self.prompt!=2:
-           self.Hide_4()
+           self.too_long+="_"+str(os.path.basename(self.input_file))+" "
+   
            
-    def Shown_4(self):
-       
-       self.lineEdit_13.show()
    
        
-    def Hide_4(self):
-        
-       self.lineEdit_13.hide()
-       
-       
-    def checkBox_9_stateChanged(self):
-    
-       self.prompt=self.checkBox_9.checkState()
-       if self.prompt==2:
-           self.Shown_9()
-       if self.prompt!=2:
-           self.Hide_9()
+
+
            
-    def Shown_9(self):
+  
        
-       self.lineEdit_14.show()
-       
-    def Hide_9(self):
-        
-       self.lineEdit_14.hide()
-       
-    def checkBox_10_stateChanged(self):
+    def checkBox_10_stateChanged(self):#remove fixed number of bases
      
        self.prompt=self.checkBox_10.checkState()
        if self.prompt==2:
@@ -247,86 +233,44 @@ class Cutadapt(QtGui.QMainWindow, form_class):
         
        self.lineEdit_2.hide()
         
+        
+       
     def demultiShown(self):
-        """
-       self.label_4.show()
-       self.lineEdit_3.show()
-       self.label_6.show()
-       self.pushButton_2.show()
-       self.lineEdit_5.show()
-       #self.label_5.show()
-       #self.lineEdit_4.show()
-       self.label_10.show()
-       self.pushButton_6.show()
-       self.pushButton_4.show()
-       """
+      
         self.frame.show()
     
     def trimShown(self):
-        """
-        self.label_7.show()
-        self.groupBox.show()
-        self.label_8.show()
-        self.checkBox_3.show()
-        self.pushButton.show()
-        self.lineEdit_6.show()
-        self.label_9.show()
-        self.pushButton_3.show()
-        self.label_11.show()
-        self.lineEdit_9.show()
-        self.lineEdit_7.show()
-        self.label_14.show()
-        self.checkBox_4.show()
-        self.checkBox_7.show()
-        self.checkBox_9.show()
-        self.checkBox_10.show()
-        self.checkBox_11.show()
-        self.label_2.show()
-        self.lineEdit.show()
-        self.checkBox.show()
-        self.pushButton_5.show()
-        self.label_3.show()
-        self.buttonBox.show()#trimHide
-        """
+       
         self.frame_2.show()
         self.lineEdit_10.hide()
         self.checkBox_6.hide()
-        self.lineEdit_11.hide()
+        
         self.lineEdit_12.hide()
         self.checkBox_8.hide()
-        self.lineEdit_13.hide()
-        self.lineEdit_14.hide()
+        
+     
         self.lineEdit_2.hide()
        
     def trimHide(self):
-        """
-        self.label_7.hide()
-        self.groupBox.hide()
-        self.label_8.hide()
-        self.checkBox_3.hide()
-        self.pushButton.hide()
-        self.lineEdit_6.hide()
-        self.label_9.hide()
-        self.pushButton_3.hide()
-        self.label_11.hide()
-        self.lineEdit_9.hide()
-        self.lineEdit_7.hide()
-        self.label_14.hide()
-        self.checkBox_4.hide()
-        self.checkBox_7.hide()
-        self.checkBox_9.hide()
-        self.checkBox_10.hide()
-        self.checkBox_11.hide()
-        self.label_2.hide()
-        self.lineEdit.hide()
-        self.checkBox.hide()
-        self.lineEdit_2.hide()
-        self.pushButton_5.hide()
-        self.label_3.hide()
-        self.buttonBox.hide()#trimHide
-        """
+        
         self.frame_2.hide()
-
+     
+    def trimming(self):
+        if self.radioButton_4.isChecked():
+           self.untrimmed_output=" --untrimmed-output untrimmed_Output_"+str(os.path.basename(self.input_file))+" "
+        if self.radioButton_7.isChecked():
+           self.discard_t=" --discard-trimmed "
+        if self.radioButton_8.isChecked():
+           self.discard_unt=" --discard-untrimmed "
+        if self.prompt_1!=2:
+            self.file_2=str(self.lineEdit_6.text())
+        if (self.radioButton_2.isChecked()|self.radioButton_5.isChecked()): 
+           self.command_1="~/.local/bin/cutadapt -g "+str(self.file_2)+" -m "+str(self.lineEdit_10.text())+str(self.too_short)+" -M "+str(self.lineEdit_12.text())+str(self.too_long)+str(self.untrimmed_output)+str(self.discard_t)+str(self.discard_unt)+" -e "+str(self.lineEdit.text())+" -u "+str(self.lineEdit_2.text())+" -o trimmed_"+str(os.path.basename(self.input_file))+" "+self.input_file 
+        if (self.radioButton.isChecked()|self.radioButton_3.isChecked()|self.radioButton_6.isChecked()):
+           self.command_1="~/.local/bin/cutadapt -a "+str(self.file_2)+" -m "+str(self.lineEdit_10.text())+str(self.too_short)+" -M "+str(self.lineEdit_12.text())+str(self.too_long)+str(self.untrimmed_output)+str(self.discard_t)+str(self.discard_unt)+" -e "+str(self.lineEdit.text())+" -u "+str(self.lineEdit_2.text())+" -o trimmed_"+str(os.path.basename(self.input_file))+" "+self.input_file
+        print self.command_1
+        subprocess.call(self.command_1,shell=True)
+        
 form = Cutadapt()
 form.show()
 app.exec_()
